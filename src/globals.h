bf2645e7c6296311ff6f2ade1caa45cee2abd21a
spinclick <jasp@debian>
2015-12-24 18:29:36 -0500

    tweak global limits

--- src/globals.h
+++ src/globals.h
@@ -2,12 +2,11 @@
 #define GLOBALS_H_
 
 #define OUTPUT_DIRECTORY "pnglitch_c_output"
-#define TIME_BEFORE_DELETION 60*1000*1000 //60 seconds 
+#define TIME_BEFORE_DELETION 90*1000*1000 //90 seconds 
 #define MAX_CONTENT_LENGTH 10*1024*1024  //max upload, 10MB
 
 // How many bytes of disk can be used at once
-#define MAX_DISK_USAGE 10*1024*1024 //10MB
-//#define MAX_DISK_USAGE 10*1024*1024 //500MB
+#define MAX_DISK_USAGE 150*1024*1024 //150MB
 
 //Need to be global for custom libpng write/read fns
 extern long long MY_PNG_READ_OFFSET;
